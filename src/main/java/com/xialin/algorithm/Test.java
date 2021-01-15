3bc16df0ccae5c2190b573dfb7ac33bf6c123933
evanxia <evanxia@yeahka.com>
2021-01-15 16:43:42 +0800

    init

---
+++ src/main/java/com/xialin/algorithm/Test.java
@@ -0,0 +1,13 @@
+package com.xialin.algorithm;
+
+/**
+ * @program: algorithm-study
+ * @description: 测试
+ * @author: Evanxia
+ * @create: 2021-01-15 16:06
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("lkhbj,jh");
+    }
+}
